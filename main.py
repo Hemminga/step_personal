07e9a281afd0fba23191012b0baf3f1f917b38da
Foppe Hemminga <foppe@hemminga.net>
2020-09-30 21:59:37 +0200

    Finished extracting tr with data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import model
+from pprint import pprint
 
 activityid = 17972
 username = "FoppeHe"
@@ -6,3 +7,11 @@ username = "FoppeHe"
 
 if __name__ == '__main__':
     data = model.get_data(activityid, username)
+    data_tables = model.get_data_table(data)
+    tables = model.extract_data(data_tables)
+    pprint(len(tables))
+    # tables[0] contains indo about the data
+    # tables[2] is the first header 'Overzicht Resultaten'
+    # tables[3] is the results table
+    # tables[4] is the second header 'Details per Spel'
+    # tables[5] is the details table
--- model.py
+++ model.py
@@ -1,8 +1,27 @@
 import requests
+from bs4 import BeautifulSoup, element
 
 
 def get_data(_activityid, _username):
     url = 'http://admin.stepbridge.nl/show.php?page='
     url += f'tournamentinfo&activityid={_activityid}&username={_username}'
     _data = requests.get(url)
-    return _data
+
+    return _data.content.replace(b'\n', b'')
+
+
+def get_data_table(_data):
+    soup = BeautifulSoup(_data, 'html.parser')
+    return soup.table.find('table')
+
+
+def extract_data(_soup):
+    tables = []
+    trs = _soup.table
+    for tr in trs.children:
+        if type(tr) != element.NavigableString:
+            if tr.name == 'tr':
+                # print(type(tr))
+                # print(tr.name)
+                tables.append(tr)
+    return tables
