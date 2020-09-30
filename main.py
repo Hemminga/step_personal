bec622e8893f4fa9bc0f5756d1fdb0e9fbcd85b7
Foppe Hemminga <foppe@hemminga.net>
2020-09-30 23:18:49 +0200

    Retrieve meta-data for the tournament

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     data_tables = model.get_data_table(data)
     tables = model.extract_data(data_tables)
     pprint(len(tables))
-    # tables[0] contains indo about the data
+    # tables[0] contains info about the data
     # tables[2] is the first header 'Overzicht Resultaten'
     # tables[3] is the results table
     # tables[4] is the second header 'Details per Spel'
     # tables[5] is the details table
+    pprint(model.get_metadata(activityid))
--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup, element
-
+from pprint import pprint
 
 def get_data(_activityid, _username):
     url = 'http://admin.stepbridge.nl/show.php?page='
     url += f'tournamentinfo&activityid={_activityid}&username={_username}'
     _data = requests.get(url)
 
-    return _data.content.replace(b'\n', b'')
+    return _data.content
 
 
 def get_data_table(_data):
@@ -25,3 +25,31 @@ def extract_data(_soup):
                 # print(tr.name)
                 tables.append(tr)
     return tables
+
+
+def get_metadata(_activityid):
+    """
+    This function gets the tournament meta data from the
+    parent page. It does all the above in this small function.
+    :param _activityid:
+    :return: dictionary with tournament meta data
+    """
+    url = 'http://admin.stepbridge.nl/show.php?page='
+    url += f'tournamentinfo&activityid={_activityid}'
+    _data = requests.get(url)
+    soup = BeautifulSoup(_data.content, 'html.parser')
+    table = soup.find('table', class_='data')
+    _data = {}
+    for tr in table.children:
+        if tr.name == 'col':
+            continue
+        if type(tr) != element.NavigableString:
+            trs = tr.find_all('td')
+            index = 0
+            for td in trs:
+                if index % 2 == 1:
+                    _data[key] = td.text.replace('\xa0\xa0', ' ')
+                else:
+                    key = td.text[:-1]
+                index += 1
+    return _data
